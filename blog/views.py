8f603476aecc1bbeca4095e1e74959b87fbcddd9
prakhar154 <mprakhar154@gmail.com>
2019-10-16 13:16:14 +0530

    added database

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-	
-	{
-	'Title' : 'Blog post 1',
-	'Author' : 'Prakhar',
-	'Date_posted' : '15th October',
-	'Content' : 'This is the first post.' 
-	},
-
-	{	
-	'Title' : 'Blog post 2',
-	'Author' : 'Prakhar',
-	'Date_posted' : '15th October',
-	'Content' : 'This is the second post.' 
-	}
-
-]
 
 def home(request):
 	context = {
-	'posts' : posts
+	'posts' : Post.objects.all()
 	}
 	return render(request, 'blog/home.html', context)
 
